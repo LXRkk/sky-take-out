d063c1745d4b486a97c0a90e31bfba13ed1d5fe8
LXRkk <luoxiurang@163.com>
2025-04-04 21:33:12 +0800

    feat: 文件上传

---
+++ sky-server/src/main/java/com/sky/config/OssConfiguration.java
@@ -0,0 +1,8 @@
+package com.sky.config;/**
+ *注释
+ *
+ *@author : LXRkk
+ *@date : 2025/4/4 21:01
+ */
+public class OssConfiguration {
+}
---
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -0,0 +1,8 @@
+package com.sky.controller.admin;/**
+ *注释
+ *
+ *@author : LXRkk
+ *@date : 2025/4/4 20:23
+ */
+public class CommonController {
+}
